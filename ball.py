5cac6b3614037043196ee13a4bdc0d42d2f86749
Ronak <ronakfeedback@gmail.com>
2024-08-01 23:41:58 +0100

    Ball Numbers

--- ball.py
+++ ball.py
@@ -1,19 +1,29 @@
 import pygame
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, color, radius, init_velo = [0, 0]):
+    def __init__(self, color, radius, value, init_velo = [0, 0]):
         super().__init__()
         
         # Set the radius of the ball
         self.radius = radius
         self.color = color
         self.velocity = init_velo
+        self.value = value
 
         # Create a surface with extra space to draw the circle
         self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
         
         # Draw the circle onto the surface
         pygame.draw.circle(self.image, self.color, (radius, radius), radius)
+
+        font_size = int(radius * 0.9)
+        font = pygame.font.Font(None, font_size)  # None uses the default font
+
+        text_surface = font.render(str(self.value), True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(radius, radius))  # Center the text on the ball
+
+        # Blit the text onto the ball's surface
+        self.image.blit(text_surface, text_rect)
 
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
